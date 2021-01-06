d8a10d2b64a0f3f8343144f7c4f66ed588a5739e
hyunjeongPark <GuoHJP@gmail.com>
2021-01-07 06:13:08 +0900

    use CryptoJS set up

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number, previousHash: string, timestamp: number, data: string
+        ): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor (
         index: number,
         hash: string,
@@ -21,9 +29,28 @@ class Block {
 
 const genestisBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-let blockChain: [Block] = [genestisBlock];
+let blockChain: Block[] = [genestisBlock];
 
 console.log(blockChain);
 
+// 블록체인이 얼마나 긴지 알아보는 것
+// --> 내 블록체인에 블록을 하나 더 추가할 수 있게하기 위해
+const getBlockChain = () : Block[] => blockChain;
+
+//  블록체인의 길이를 알아야함(블록체인 안에서 가장 최근의 블록을 리턴)
+const getLatestBlock = () : Block => blockChain[blockChain.length-1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
 export {};
 
+// 새로운 블록 만들기 위해선 hash 필요함
+// hash: 모든 속성을 길고 수학적으로 하나의 문자열로 결합한 것
+// cryptoJS 설치 --> yarn add crypto-js
+// cryptoJS 임포트 --> import * as CryptoJS from "crypto=js";
+// 블록의 hash를 계산해야함
+// --> Block class 안에 static method 만듦
+// static이란? class가 생성되지 않아도 호출할 수 있는 method 
+//            --> Block class안에서 항상 사용 가능한 method
+// static 없으면 Block class 밖에서 'Block.함수명' 이런 식으로 호출할 수 없음
+// static 없이 class 밖에서 함수 호출 위해선 Block을 지정해야함(genesisBlock처럼)
